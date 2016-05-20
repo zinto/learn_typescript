9806277a6326d1b24cc790c544db5f500438424c
Nguyen Anh Dao <dao.nguyen@smoovapp.com>
2016-05-21 00:19:04 +0700

    add basic type

---
+++ src/basicTypes.ts
@@ -0,0 +1,22 @@
+// Boolean
+let isDone: boolean = false;
+
+// Number
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+//String 
+let color: string = "blue";
+let fullName: string = "Anh Tuan";
+let age: number = 37;
+let sentence: string = `hello, my name is ${fullName}`
+
+let sentence1: string =`I'll be ${age + 1} years old next month`
+
+// Array
+let list: number[] = [1, 2, 3];
+
+// Generic Array
+let genericArray : Array<number> = [1,2,3];
---
+++ src/scripts/basicTypes.js
@@ -0,0 +1,17 @@
+// Boolean
+var isDone = false;
+// Number
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+//String 
+var color = "blue";
+var fullName = "Anh Tuan";
+var age = 37;
+var sentence = "hello, my name is " + fullName;
+var sentence1 = "I'll be " + (age + 1) + " years old next month";
+// Array
+var list = [1, 2, 3];
+// Generic Array
+var genericArray = [1, 2, 3];
