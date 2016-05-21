3db13890ae306fffbb11a3bfc722da63069187bb
Nguyen Anh Dao <dao.nguyen@smoovapp.com>
2016-05-21 21:38:28 +0700

    update interface.ts

--- src/interface.ts
+++ src/interface.ts
@@ -38,3 +38,60 @@ var shape = {
 }
 
 shape.popup();
+//---------------------------
+// BEGINING
+interface SquareConfig{
+    color?: string;
+    width?: number;
+    [propName: string]: any;
+}
+function createSquare(config: SquareConfig) : {color: string; area: number} {
+    let newSquare = {color: "white", area: 100};
+    
+    if (config.color) {
+        newSquare.color = config.color;
+        
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+        
+    }
+    return newSquare;
+}
+let mySquare = createSquare({color: "black"});
+
+/**
+ * Excess Property Checks
+ */
+let mySquare = createSquare({ colour: "red", width: 100 });
+// error: 'colour' not expected in type 'SquareConfig'
+let mySquare = createSquare({ colour: "red", width: 100 });
+let mySquare = createSquare({ width: 100, opacity: 0.5 } as SquareConfig);
+//
+let squareOptions = { colour: "red", width: 100 };
+let mySquare = createSquare(squareOptions);
+/**
+ * Function Types
+ * 
+ * To describe a function type with an interface, we give the interface a call signature. 
+ * This is like a function declaration with only the parameter list and return type given. 
+ * Each parameter in the parameter list requires both name and type.
+ */
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+/**
+ * Once defined, we can use this function type interface like we would other interfaces. 
+ * Here, we show how you can create a variable of a function type 
+ * and assign it a function value of the same type.
+ */
+let mySearch: SearchFunc;
+mySearch = function(source: string, subString: string) {
+    let result = source.search(subString);
+    if (result == -1) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
--- src/scripts/interface.js
+++ src/scripts/interface.js
@@ -17,3 +17,39 @@ var shape = {
     }
 };
 shape.popup();
+function createSquare(config) {
+    var newSquare = { color: "white", area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySquare = createSquare({ color: "black" });
+/**
+ * Excess Property Checks
+ */
+var mySquare = createSquare({ colour: "red", width: 100 });
+// error: 'colour' not expected in type 'SquareConfig'
+var mySquare = createSquare({ colour: "red", width: 100 });
+var mySquare = createSquare({ width: 100, opacity: 0.5 });
+//
+var squareOptions = { colour: "red", width: 100 };
+var mySquare = createSquare(squareOptions);
+/**
+ * Once defined, we can use this function type interface like we would other interfaces.
+ * Here, we show how you can create a variable of a function type
+ * and assign it a function value of the same type.
+ */
+var mySearch;
+mySearch = function (source, subString) {
+    var result = source.search(subString);
+    if (result == -1) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
